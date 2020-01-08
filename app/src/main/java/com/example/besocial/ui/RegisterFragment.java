d19fa4f8144ca2dbda755a37d6b14d84540f4439
naor555666 <naor555666@gmail.com>
2020-01-08 11:23:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/besocial/ui/RegisterFragment.java
@@ -0,0 +1,135 @@
+package com.example.besocial.ui;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import com.example.besocial.MainActivity;
+import com.example.besocial.R;
+import com.example.besocial.ui.login.LoginActivity;
+import com.example.besocial.ui.login.LoginFragment;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterFragment extends Fragment {
+    private static final RegisterFragment registerFragment = new RegisterFragment();
+
+    public static RegisterFragment getInstance() {
+        return registerFragment;
+    }
+
+    private RegisterFragment() {
+
+    }
+
+    private EditText firstName, lastName, email, confirmEmail, password, confirmPassword;
+    private Button clearFields, createAccount;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firstName = view.findViewById(R.id.firstName);
+        lastName = view.findViewById(R.id.lastName);
+        email = view.findViewById(R.id.username);
+        confirmEmail = view.findViewById(R.id.confirmUsername);
+        password = view.findViewById(R.id.password);
+        confirmPassword = view.findViewById(R.id.confirmPassword);
+        clearFields = view.findViewById(R.id.clearFieldsRegister);
+        createAccount = view.findViewById(R.id.createAccount);
+        setListeners();
+
+    }
+
+
+    private class ClickListener implements View.OnClickListener {
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.clearFieldsRegister:
+                    clearFields();
+                    break;
+                case R.id.createAccount:
+                    if (checkFields()) {
+                        Toast.makeText(getActivity(), "Registered successfully", Toast.LENGTH_SHORT).show();
+                    } else
+
+                    break;
+            }
+        }
+    }
+
+    private boolean checkFields() {
+        Toast.makeText(getActivity(), "chekc fielda", Toast.LENGTH_SHORT);
+
+        Boolean isFieldsValid = false;
+        String firstNameString = firstName.getText().toString();
+        String lastNameString = lastName.getText().toString();
+        String emailString = email.getText().toString();
+        String confirmEmailString = confirmEmail.getText().toString();
+        String passwordString = password.getText().toString();
+        String confirmPasswordString = confirmPassword.getText().toString();
+
+        if (firstNameString.equals("")) {
+            Toast.makeText(getActivity(), "First name is empty", Toast.LENGTH_SHORT).show();
+        } else if (lastNameString.equals("")) {
+            Toast.makeText(getActivity(), "Last name is empty", Toast.LENGTH_SHORT).show();
+        } else if (emailString.equals("")) {
+            Toast.makeText(getActivity(), "Email is empty", Toast.LENGTH_SHORT).show();
+        } else if (confirmEmailString.equals("")) {
+            Toast.makeText(getActivity(), "Email confirmation is empty", Toast.LENGTH_SHORT).show();
+        } else if (passwordString.equals("")) {
+            Toast.makeText(getActivity(), "Password is empty", Toast.LENGTH_SHORT).show();
+        } else if (confirmPasswordString.equals("")) {
+            Toast.makeText(getActivity(), "Password confirmation is empty", Toast.LENGTH_SHORT).show();
+        } else if (!emailString.equals(confirmEmailString)) {
+            Toast.makeText(getActivity(), "Email confirmation is incompatible", Toast.LENGTH_SHORT).show();
+        } else if (!passwordString.equals(confirmPasswordString)) {
+            Toast.makeText(getActivity(), "Password confirmation is incompatible", Toast.LENGTH_SHORT).show();
+        } else
+            isFieldsValid = true;
+
+        return isFieldsValid;
+    }
+
+    private void clearFields() {
+        firstName.setText("");
+        lastName.setText("");
+        email.setText("");
+        confirmEmail.setText("");
+        password.setText("");
+        confirmPassword.setText("");
+    }
+
+    private void setListeners() {
+        View.OnClickListener clickListener = new ClickListener();
+        clearFields.setOnClickListener(clickListener);
+        createAccount.setOnClickListener(clickListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LoginFragment loginFragment = LoginFragment.getInstance();
+        loginFragment.setRegisterValue(true);
+    }
+}
---
+++ app/src/main/java/com/example/besocial/ui/login/LoginFragment.java
@@ -0,0 +1,167 @@
+package com.example.besocial.ui.login;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.besocial.MainActivity;
+import com.example.besocial.R;
+import com.example.besocial.ui.RegisterFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    private EditText email, password;
+    private Button register, login;
+    private FirebaseAuth firebaseAuth;
+    private CheckBox checkBox;
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor editor;
+
+    private static  LoginFragment loginFragment;
+    public static LoginFragment getInstance(){return loginFragment;}
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        loginFragment= this;
+        editor = sharedPref.edit();
+        email = (EditText) view.findViewById(R.id.username);
+        password = (EditText) view.findViewById(R.id.password);
+        register = view.findViewById(R.id.register);
+        login = view.findViewById(R.id.login);
+        login.setEnabled(true);
+        setListeners();
+        checkBox = (CheckBox) view.findViewById(R.id.remember_emailPassword_checkBox);
+        firebaseAuth= FirebaseAuth.getInstance();
+        boolean isCheckBoxChecked = false;
+        isCheckBoxChecked = sharedPref.getBoolean("remember_emailPassword_checkBox", false);
+        checkBox.setChecked(isCheckBoxChecked);
+        if (isCheckBoxChecked) {
+            String emailS = sharedPref.getString("email", null);
+            String passwordS = sharedPref.getString("password", null);
+
+            email.setText(emailS);
+            password.setText(passwordS);
+        }
+    }
+
+    private void setListeners() {
+        View.OnClickListener clickListener = new ClickListener();
+        login.setOnClickListener(clickListener);
+        register.setOnClickListener(clickListener);
+    }
+
+
+
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        getActivity().finish();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        saveLoginPreference();
+    }
+
+    /**
+     * save user email and password, checkbox - using shared preferances
+     */
+
+    private void saveLoginPreference() {
+        editor.putBoolean("remember_emailPassword_checkBox", checkBox.isChecked());
+        editor.putString("email", email.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.commit();
+    }
+
+    private class ClickListener implements View.OnClickListener {
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.register:
+                    register.setEnabled(false);
+                    //Toast.makeText(this, "register", Toast.LENGTH_SHORT).show();
+                    getFragmentManager().beginTransaction().
+                            replace(R.id.loginContainer,  RegisterFragment.getInstance()).//add on top of the static fragment
+                            addToBackStack("BBB").//cause the back button scrolling through the loaded fragments
+                            commit();
+                    getFragmentManager().executePendingTransactions();
+                    break;
+                case R.id.login:
+                    login.setEnabled(false);
+                    loginUser();
+                    break;
+            }
+        }
+    }
+
+    private void loginUser() {
+        String usernameString = email.getText().toString();
+        String passwordString = password.getText().toString();
+        if (usernameString.isEmpty())
+            Toast.makeText(getActivity(), "Username field is empty", Toast.LENGTH_SHORT).show();
+        else if (passwordString.isEmpty())
+            Toast.makeText(getActivity(), "Password field is empty", Toast.LENGTH_SHORT).show();
+        else {
+            firebaseAuth.signInWithEmailAndPassword(usernameString, passwordString)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                sendUserToMainActivity();
+                                Toast.makeText(getActivity(), "Logged in successfuly", Toast.LENGTH_SHORT).show();
+                            } else {
+                                login.setEnabled(true);
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(getActivity(), "Could not log in: " + errorMessage, Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+
+                    });
+        }
+
+
+           //getActivity().finish();
+    }
+
+    public void setRegisterValue(boolean registerValue) {
+        this.register.setEnabled(registerValue);
+    }
+}
