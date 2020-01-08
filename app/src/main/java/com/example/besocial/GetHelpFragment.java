5762e06d4baef458ef8d61b8f60513eae041a08a
OrMagogi <sdgijhdbfsabc@gmail.com>
2020-01-08 16:01:41 +0200

    create progress dialog

---
+++ app/src/main/java/com/example/besocial/GetHelpFragment.java
@@ -0,0 +1,31 @@
+package com.example.besocial;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GetHelpFragment extends Fragment {
+
+
+    public GetHelpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_get_help, container, false);
+    }
+
+}
