e5b7aafdf4580918a507a932cf7a8058cdec7445
OrMagogi <sdgijhdbfsabc@gmail.com>
2020-01-11 12:28:39 +0200

    create progress dialog

--- app/src/main/java/com/example/besocial/ui/RegisterFragment.java
+++ app/src/main/java/com/example/besocial/ui/RegisterFragment.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,8 @@ import com.example.besocial.R;
 import com.example.besocial.ui.login.LoginActivity;
 import com.example.besocial.ui.login.LoginFragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterFragment extends Fragment {
     private static final RegisterFragment registerFragment = new RegisterFragment();
 
@@ -37,7 +41,8 @@ public class RegisterFragment extends Fragment {
 
     private EditText firstName, lastName, email, confirmEmail, password, confirmPassword;
     private Button clearFields, createAccount;
-
+    private FirebaseDatabase database=FirebaseDatabase.getInstance();
+    private RegisterTextWatcher registerTextWatcher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -119,11 +124,59 @@ public class RegisterFragment extends Fragment {
         password.setText("");
         confirmPassword.setText("");
     }
+    public class RegisterTextWatcher implements android.text.TextWatcher{
+        private int chosenEditText;
+        public RegisterTextWatcher(int chosenEditText){
+            super();
+            this.chosenEditText=chosenEditText;
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if(chosenEditText==firstName.getId()){
+
+            }
+            else if(chosenEditText==lastName.getId()){
+
+            }
+            else if(chosenEditText==email.getId()){
+
+            }
+            else if(chosenEditText==confirmEmail.getId()){
+
+            }
+            else if(chosenEditText==password.getId()){
+                if(password.getText().length()<8)
+                    Toast.makeText(getActivity(), "Password must have 8 characters or more", Toast.LENGTH_SHORT).show();
+            }
+            else if(chosenEditText==confirmPassword.getId()){
+
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
 
     private void setListeners() {
         View.OnClickListener clickListener = new ClickListener();
         clearFields.setOnClickListener(clickListener);
         createAccount.setOnClickListener(clickListener);
+        firstName.addTextChangedListener(new RegisterTextWatcher(firstName.getId()));
+        lastName.addTextChangedListener(new RegisterTextWatcher(lastName.getId()));
+        email.addTextChangedListener(new RegisterTextWatcher(email.getId()));
+        confirmEmail.addTextChangedListener(new RegisterTextWatcher(confirmEmail.getId()));
+        password.addTextChangedListener(new RegisterTextWatcher(password.getId()));
+        confirmPassword.addTextChangedListener(new RegisterTextWatcher(confirmPassword.getId()));
+
+
     }
 
     @Override
