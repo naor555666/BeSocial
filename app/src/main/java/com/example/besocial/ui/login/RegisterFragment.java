3d0fad3981f1287d14ce7e65c2be7acaaee8ae08
naor555666 <naor555666@gmail.com>
2020-01-11 20:13:27 +0200

    register operate

--- app/src/main/java/com/example/besocial/ui/login/RegisterFragment.java
+++ app/src/main/java/com/example/besocial/ui/login/RegisterFragment.java
@@ -1,10 +1,11 @@
-package com.example.besocial.ui;
+package com.example.besocial.ui.login;
 
 
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,9 +22,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.besocial.MainActivity;
 import com.example.besocial.R;
-import com.example.besocial.ui.login.LoginActivity;
-import com.example.besocial.ui.login.LoginFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class RegisterFragment extends Fragment {
     private static final RegisterFragment registerFragment = new RegisterFragment();
 
@@ -38,6 +45,18 @@ public class RegisterFragment extends Fragment {
     private EditText firstName, lastName, email, confirmEmail, password, confirmPassword;
     private Button clearFields, createAccount;
 
+    private String firstNameString;
+    private String lastNameString;
+    private String emailString;
+    private String confirmEmailString;
+    private String passwordString;
+    private String confirmPasswordString;
+
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference userRef;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,14 +68,19 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        firstName = view.findViewById(R.id.firstName);
-        lastName = view.findViewById(R.id.lastName);
-        email = view.findViewById(R.id.username);
-        confirmEmail = view.findViewById(R.id.confirmUsername);
-        password = view.findViewById(R.id.password);
-        confirmPassword = view.findViewById(R.id.confirmPassword);
+        firstName = (EditText) view.findViewById(R.id.firstName);
+        lastName = (EditText) view.findViewById(R.id.lastName);
+        email = (EditText) view.findViewById(R.id.usernameRegister);
+        confirmEmail = (EditText) view.findViewById(R.id.confirmUsername);
+        password = (EditText) view.findViewById(R.id.passwordRegister);
+        confirmPassword = (EditText) view.findViewById(R.id.confirmPassword);
         clearFields = view.findViewById(R.id.clearFieldsRegister);
         createAccount = view.findViewById(R.id.createAccount);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
         setListeners();
 
     }
@@ -70,12 +94,47 @@ public class RegisterFragment extends Fragment {
                     break;
                 case R.id.createAccount:
                     if (checkFields()) {
-                        Toast.makeText(getActivity(), "Registered successfully", Toast.LENGTH_SHORT).show();
-                    } else
+                        firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).
+                                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            String userID = task.getResult().getUser().getUid();
+                                            saveUserDetails(userID);
+                                        } else {
+                                            String errorMessage = task.getException().getMessage();
+                                            Toast.makeText(getActivity(), "Could not register: " + errorMessage, Toast.LENGTH_LONG).show();
 
+                                        }
+                                    }
+
+
+                                });
+                    }
+                    break;
+                default:
                     break;
             }
         }
+    }
+
+    private void saveUserDetails(String userID) {
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+        HashMap userMap = new HashMap();
+        userMap.put("userFirstName", firstName.getText().toString());
+        userMap.put("userLastName", lastName.getText().toString());
+        userRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getActivity(), "Registered successfully", Toast.LENGTH_SHORT).show();
+                    getFragmentManager().popBackStack();
+                } else {
+                    String errorMessage = task.getException().getMessage();
+                    Toast.makeText(getActivity(), "Could not fill details: " + errorMessage, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private boolean checkFields() {
