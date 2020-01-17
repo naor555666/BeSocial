4286ebfb5070516bdf45717989724a7ce0a1c980
naor555666 <naor555666@gmail.com>
2020-01-17 21:44:21 +0200

    social center was editted

--- app/src/main/java/com/example/besocial/ui/SocialCenterFragment.java
+++ app/src/main/java/com/example/besocial/ui/SocialCenterFragment.java
@@ -3,20 +3,28 @@ package com.example.besocial.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.besocial.MainActivity;
 import com.example.besocial.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SocialCenterFragment extends Fragment {
-
+public class SocialCenterFragment extends Fragment implements View.OnClickListener {
+    Button socialEventButton, bonusAreaButton, volunteerButton, getHelpButton;
+    NavController navController;
 
     public SocialCenterFragment() {
         // Required empty public constructor
@@ -30,4 +38,29 @@ public class SocialCenterFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_social_center, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        socialEventButton = view.findViewById(R.id.socialEventButton);
+        bonusAreaButton = view.findViewById(R.id.bonusAreaButton);
+        volunteerButton = view.findViewById(R.id.volunteerButton);
+        getHelpButton = view.findViewById(R.id.getHelpButton);
+        setListeners();
+        navController = MainActivity.getNavController();
+    }
+
+    private void setListeners() {
+        socialEventButton.setOnClickListener(this);
+        bonusAreaButton.setOnClickListener(this);
+        volunteerButton.setOnClickListener(this);
+        getHelpButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.getHelpButton) {
+            navController.navigate(R.id.action_nav_social_center_to_createEventFragment);
+        }
+    }
 }
