c156e4421f087521cf2aa5f8e4b1b073d76e8515
naor555666 <naor555666@gmail.com>
2020-01-18 20:25:32 +0200

    Event class was added + fragment_event_list was modified

--- app/src/main/java/com/example/besocial/ui/HomeFragment.java
+++ app/src/main/java/com/example/besocial/ui/HomeFragment.java
@@ -4,8 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
