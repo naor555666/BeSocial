00cc0ac79bd10111b07909802b769d22c1ca5559
OrMagogi <sdgijhdbfsabc@gmail.com>
2020-03-18 20:16:03 +0200

    profile and chat improvements

--- app/src/main/java/com/example/besocial/ui/ProfileFragment.java
+++ app/src/main/java/com/example/besocial/ui/ProfileFragment.java
@@ -3,20 +3,28 @@ package com.example.besocial.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.besocial.MainActivity;
 import com.example.besocial.R;
+import com.example.besocial.data.User;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
-    
+    private TextView profilePageUsername;
+    private EditText profileFullName,profileEmail;
+    User loggedUser;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -30,4 +38,15 @@ public class ProfileFragment extends Fragment {
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loggedUser=MainActivity.getLoggedUser();
+        profilePageUsername=view.findViewById(R.id.profile_page_username);
+        profileFullName=view.findViewById(R.id.profile_full_name);
+        profileEmail=view.findViewById(R.id.profile_email);
+        profilePageUsername.setText(loggedUser.getUserFirstName()+"  "+loggedUser.getUserLastName());
+        profileEmail.setText(loggedUser.getUserEmail());
+        profileFullName.setText(loggedUser.getUserFirstName()+"  "+loggedUser.getUserLastName());
+    }
 }
