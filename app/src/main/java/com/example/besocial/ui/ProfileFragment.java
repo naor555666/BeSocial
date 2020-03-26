13204d30aa302d46c2b2840f1c28f39cad80b3bf
OrMagogi <sdgijhdbfsabc@gmail.com>
2020-03-26 16:52:36 +0200

    change profile picture without cropping

--- app/src/main/java/com/example/besocial/ui/ProfileFragment.java
+++ app/src/main/java/com/example/besocial/ui/ProfileFragment.java
@@ -1,6 +1,10 @@
 package com.example.besocial.ui;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,20 +15,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.besocial.MainActivity;
 import com.example.besocial.R;
 import com.example.besocial.data.User;
+import com.google.firebase.storage.StorageReference;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+    private CircleImageView profileProfilePicture;
     private TextView profilePageUsername;
-    private EditText profileFullName,profileEmail;
-    User loggedUser;
+    private EditText profileFullName,profileEmail,profileCity,profileAddress,profileBirthday;
+    private User loggedUser;
+    private final static int galleryPick=1;
+    private ImageButton profileChangeProfilePicture,profileEditProfileDetails;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -42,11 +56,64 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         loggedUser=MainActivity.getLoggedUser();
+        profileProfilePicture=view.findViewById(R.id.profile_user_profile_picture);
+        profileChangeProfilePicture=view.findViewById(R.id.profile_change_profile_picture);
         profilePageUsername=view.findViewById(R.id.profile_page_username);
         profileFullName=view.findViewById(R.id.profile_full_name);
+        profileAddress=view.findViewById(R.id.profile_adress);
+        profileCity=view.findViewById(R.id.profile_city);
         profileEmail=view.findViewById(R.id.profile_email);
+        profileBirthday=view.findViewById(R.id.profile_birthday);
+        profileEditProfileDetails=view.findViewById(R.id.profile_edit_profile_details);
         profilePageUsername.setText(loggedUser.getUserFirstName()+"  "+loggedUser.getUserLastName());
         profileEmail.setText(loggedUser.getUserEmail());
         profileFullName.setText(loggedUser.getUserFirstName()+"  "+loggedUser.getUserLastName());
+        profileEditProfileDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profileAddress.setEnabled(true);
+                profileCity.setEnabled(true);
+                profileBirthday.setEnabled(true);
+                profileFullName.setEnabled(true);
+            }
+        });
+
+        profileChangeProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent=new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                //CropImage.activity()
+                      //  .start(getView().getContext(), this);
+
+                 startActivityForResult(galleryIntent,galleryPick);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==galleryPick && resultCode== Activity.RESULT_OK && data!=null){
+            Uri imageUri=data.getData();
+           // CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).setAspectRatio(1,1).start(getActivity());
+            profileProfilePicture.setImageURI(imageUri);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == Activity.RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Toast.makeText(getActivity(), "Change profile successfuly", Toast.LENGTH_LONG).show();
+
+                profileProfilePicture.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+
     }
 }
